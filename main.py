a23e709cf935417fa70834902d97b7a200ac86d8
Amansaxena16 <aman16nov.as@gmail.com>
2024-03-26 00:07:33 +0530

    Added Encryption Feature

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from cryptography.fernet import Fernet
+
+# Reading the Key File
+def load_key():
+    file = open('key.key','rb')
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+
+fer = Fernet(key)
+
 mode = input("Would you like to add a new password or view another(View/Add) - ").lower()
 
 def view():
@@ -5,7 +18,7 @@ def view():
     for line in file.readlines():
         data = line.rstrip()
         user, password = data.split("|")
-        print("User Name = ",user, '|',"password = ",password)
+        print("User Name = ",user, '|',"password = ",fer.decrypt(password.encode()).decode())
     file.close()
 
 def add():
@@ -13,7 +26,7 @@ def add():
     password = input("Enter your Password!! ")
 
     file = open('Password.txt','a')
-    file.write(email + "|" + password + "\n")
+    file.write(email + "|" + fer.encrypt(password.encode()).decode() + "\n")
     file.close()
 
 while(True):
@@ -33,6 +46,13 @@ while(True):
         print("Invalid Input!!")
         continue
 
+
+# --> This function() is used to create the Key for encrytion!! 
+# def write_key():
+#     key = Fernet.generate_key()
+#     key_file =  open('key.key','wb')  
+#     key_file.write(key)
+#     key_file.close()
 
 
 
