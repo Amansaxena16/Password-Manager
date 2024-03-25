522c4ee0d4c9f1f0899fcfd64c5d50cae8a2dbbc
Amansaxena16 <aman16nov.as@gmail.com>
2024-03-26 00:17:27 +0530

    Added Encryption Feature

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
+# Fernet --> Fernet is a symmetric encryption algorithm provided by the cryptography library in Python. It requires a secret key to encrypt and decrypt data.
 from cryptography.fernet import Fernet
 
 # Reading the Key File
 def load_key():
-    file = open('key.key','rb')
-    key = file.read()
+    file = open('key.key','rb') 
+    key = file.read() # Here reading the file in read binary form and storing in key variable
     file.close()
     return key
 
-key = load_key()
-
+key = load_key() # function call and storing data
 fer = Fernet(key)
 
+# Asking to View or to Add the Password
 mode = input("Would you like to add a new password or view another(View/Add) - ").lower()
 
+# View Function to see the passwords
 def view():
     file = open("Password.txt","r")
     for line in file.readlines():
-        data = line.rstrip()
-        user, password = data.split("|")
-        print("User Name = ",user, '|',"password = ",fer.decrypt(password.encode()).decode())
+        data = line.rstrip()  # Removing all the white spacing and newline with rstrip()
+        user, password = data.split("|")  #spliting the username and password will split()
+        print("User Name = ",user, '|',"password = ",fer.decrypt(password.encode()).decode()) # Decrypting the data
     file.close()
 
+# Add Function to add all the password into the file
 def add():
-    email = input("Enter Your Email!! ")
-    password = input("Enter your Password!! ")
+    email = input("Enter Your Email!! ") # taking Input from user
+    password = input("Enter your Password!! ") # taking Input from user
 
-    file = open('Password.txt','a')
-    file.write(email + "|" + fer.encrypt(password.encode()).decode() + "\n")
+    file = open('Password.txt','a') # Here creating a pass.txt file in append mode
+    file.write(email + "|" + fer.encrypt(password.encode()).decode() + "\n") # writing the data into the file in encryted form using Fernet
     file.close()
 
 while(True):
@@ -48,6 +51,7 @@ while(True):
 
 
 # --> This function() is used to create the Key for encrytion!! 
+    
 # def write_key():
 #     key = Fernet.generate_key()
 #     key_file =  open('key.key','wb')  
